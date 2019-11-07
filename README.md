ff17b3d32a70a5664ae21c06e4debb7bbd12f165
enghamzasalem <enghamzasalem@yahoo.com>
2019-11-07 23:30:45 +0300

    update .rm

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Inn_DMD_Phase3
 This is a repo for innopolis Student for phase in DMD
 simple script writtern by python3 to interact with postgress database 
+Install pgAdmin V4 from here :
+https://www.postgresql.org/ftp/pgadmin/pgadmin4/v4.14/windows/
--- createdb.py
+++ createdb.py
@@ -2,27 +2,27 @@
 # psql -d template1
 import psycopg2
 # https://stackabuse.com/working-with-postgresql-in-python/
-con = psycopg2.connect(database="postgres", user="postgres",
-                       password="myPassword", host="127.0.0.1", port="5432")
+con = psycopg2.connect(database="students", user="postgres",
+                       password="123456", host="127.0.0.1", port="5432")
 
 print("Database opened successfully")
 cur = con.cursor()
-# cur.execute('''CREATE TABLE STUDENT
-#       (ADMISSION INT PRIMARY KEY     NOT NULL,
-#       NAME           TEXT    NOT NULL,
-#       AGE            INT     NOT NULL,
-#       COURSE        CHAR(50),
-#       DEPARTMENT        CHAR(50));''')
-# print("Table created successfully")
+cur.execute('''CREATE TABLE STUDENT
+      (ADMISSION INT PRIMARY KEY     NOT NULL,
+      NAME           TEXT    NOT NULL,
+      AGE            INT     NOT NULL,
+      COURSE        CHAR(50),
+      DEPARTMENT        CHAR(50));''')
+print("Table created successfully")
 
-# cur.execute("INSERT INTO STUDENT (ADMISSION,NAME,AGE,COURSE,DEPARTMENT) VALUES (3420, 'John', 18, 'Computer Science', 'ICT')")
-# cur.execute("INSERT INTO STUDENT (ADMISSION,NAME,AGE,COURSE,DEPARTMENT) VALUES (3421, 'Joel', 17, 'Computer Science', 'ICT')")
-# cur.execute("INSERT INTO STUDENT (ADMISSION,NAME,AGE,COURSE,DEPARTMENT) VALUES (3422, 'Antony', 19, 'Electrical Engineering', 'Engineering')")
-# cur.execute("INSERT INTO STUDENT (ADMISSION,NAME,AGE,COURSE,DEPARTMENT) VALUES (3423, 'Alice', 18, 'Information Technology', 'ICT')")
-# cur.execute("UPDATE STUDENT set AGE = 20 where ADMISSION = 3420")
-# cur.execute("DELETE from STUDENT where ADMISSION=3420;")
-# con.commit()
-# print("Record inserted successfully")
+cur.execute("INSERT INTO STUDENT (ADMISSION,NAME,AGE,COURSE,DEPARTMENT) VALUES (3420, 'John', 18, 'Computer Science', 'ICT')")
+cur.execute("INSERT INTO STUDENT (ADMISSION,NAME,AGE,COURSE,DEPARTMENT) VALUES (3421, 'Joel', 17, 'Computer Science', 'ICT')")
+cur.execute("INSERT INTO STUDENT (ADMISSION,NAME,AGE,COURSE,DEPARTMENT) VALUES (3422, 'Antony', 19, 'Electrical Engineering', 'Engineering')")
+cur.execute("INSERT INTO STUDENT (ADMISSION,NAME,AGE,COURSE,DEPARTMENT) VALUES (3423, 'Alice', 18, 'Information Technology', 'ICT')")
+cur.execute("UPDATE STUDENT set AGE = 20 where ADMISSION = 3420")
+cur.execute("DELETE from STUDENT where ADMISSION=3420;")
+con.commit()
+print("Record inserted successfully")
 cur = con.cursor()
 cur.execute("SELECT admission, name, age, course, department from STUDENT")
 rows = cur.fetchall()
