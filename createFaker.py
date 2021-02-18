c0aa8c6f4ff051e62f3a13418e1087eac5a7fd89
enghamzasalem <enghamzasalem@yahoo.com>
2021-02-18 14:49:08 +0300

    add faker

---
+++ createFaker.py
@@ -0,0 +1,24 @@
+# create db
+# psql -d template1
+import psycopg2
+from faker import Faker
+# https://stackabuse.com/working-with-postgresql-in-python/
+con = psycopg2.connect(database="customers", user="postgres",
+                       password="123456", host="127.0.0.1", port="5432")
+
+print("Database opened successfully")
+cur = con.cursor()
+cur.execute('''CREATE TABLE CUSTOMER
+       (ID INT PRIMARY KEY     NOT NULL,
+       Name           TEXT    NOT NULL,
+       Address            TEXT     NOT NULL,
+       review        TEXT);''')
+print("Table created successfully")
+fake = Faker()
+for i in range(100000):
+    print(i)
+    cur.execute("INSERT INTO CUSTOMER (ID,Name,Address,review) VALUES ('"+ str(i)+"','"+fake.name()+"','"+fake.address()+"','"+fake.text()+"')")
+    con.commit()
+ 
+
+#explain select * from customer
